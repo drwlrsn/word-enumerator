70a51f19527677fb058b707deba0007800eccc82
Drew Larson <me@drewlarson.ca>
2022-08-10 10:48:21 -0600

    support some command line options

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,67 @@
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
-module Main where
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-import Lib
-import System.IO (openFile, IOMode (ReadMode), hGetLine, Handle, hClose, hFileSize)
-import Data.Text (split, pack, unpack, Text, null, empty)
-import GHC.TypeLits (ErrorMessage(Text))
-import Prelude hiding (null)
-import System.IO.Error (catchIOError, isEOFError)
+module Main (main) where
 
-splitOnSpace :: Text -> [Text]
-splitOnSpace = split (\c -> c == ' ' || c == '\t')
+import           Data.Semigroup ((<>))
+import           Options.Applicative (Applicative(pure), (<$>), argument, help
+                                    , idm, short, str, switch, Parser, helper
+                                    , (<**>), fullDesc, progDesc, header
+                                    , execParser, info)
+import           System.IO (Handle, IOMode(ReadMode), hClose, hFileSize
+                          , hGetLine, openFile)
+import           Lib (getResult, Flags(Flags) , countFileWords)
 
-countLineWords = length . splitOnSpace
+data CommandOptions =
+  CommandOptions { c :: Bool
+                 , l :: Bool
+                 , m :: Bool
+                 , w :: Bool
+                 , optFileName :: String
+                 }
 
-countFileWords :: Int -> Handle -> IO Int
-countFileWords count handle =  do
-   line <- pack <$> catchIOError (hGetLine handle) (\e -> if isEOFError e then return "" else ioError e)
-   if null line then pure count else  countFileWords (count + countLineWords line) handle
-
-
+optsParser :: Parser CommandOptions
+optsParser = do
+  c <- switch
+    (short 'c'
+     <> help
+       "The number of bytes in each input file is written to the standard output.  This will cancel out any prior usage of the -m option.")
+  l <- switch
+    (short 'l'
+     <> help
+       "The number of lines in each input file is written to the standard output.")
+  m <- switch
+    (short 'm'
+     <> help
+       "The number of characters in each input file is written to the standard output.  If the current locale does not support multibyte characters, this is equivalent to the -c option.  This will cancel out any prior usage of the -c option.")
+  w <- switch
+    (short 'w'
+     <> help
+       "The number of words in each input file is written to the standard output.")
+  optFileName <- argument str idm
+  pure CommandOptions { .. }
 
 main :: IO ()
-main =  do
-  handle <- openFile "test.txt" ReadMode
-  count <- countFileWords 0 handle
+main = do
+  o <- execParser opts
+  let fileName = optFileName o
+  handle <- openFile fileName ReadMode
+  (wordCount, lineCount, charCount) <- countFileWords 0 0 0 handle
   size <- hFileSize handle
   _ <- hClose handle
-  putStrLn $ "\t" ++ show count ++ "\t" ++ show size
+  putStrLn
+    (getResult
+    (Flags (c o) (l o) (m o) (w o))
+    (toInteger wordCount)
+    (toInteger lineCount)
+    (toInteger charCount)
+    size
+    fileName)
+  where
+    opts = info
+      (optsParser <**> helper)
+      (fullDesc
+       <> progDesc
+         "The wc utility displays the number of lines, words, and bytes contained in each input file, or standard input (if no file is specified) to the standard output.  A line is defined as a string of characters delimited by a ⟨newline⟩ character.  Characters beyond the final ⟨newline⟩ character will not be included in the line count."
+       <> header "word-enumerator – word, line, character, and byte count")
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,55 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (getResult, Flags(Flags), countFileWords) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.List (intercalate)
+import           Data.Text (Text, split, pack, unpack, null)
+import           System.IO (Handle, hGetLine)
+import           System.IO.Error (catchIOError, isEOFError)
+import           Prelude hiding (null)
+
+data Flags =
+  Flags { optc :: Bool
+          -- "The number of bytes in each input file is written to the standard output.  This will cancel out any prior usage of the -m option."
+        , optl :: Bool
+          -- "The number of lines in each input file is written to the standard output."
+        , optm :: Bool
+          -- "The number of characters in each input file is written to the standard output.  If the current locale does not support multibyte characters, this is equivalent to the -c option.  This will cancel out any prior usage of the -c option."
+        , optw :: Bool -- "The number of words in each input file is written to the standard output."
+        }
+
+tab :: [Char]
+tab = "       "
+getResult
+  :: Flags -> Integer -> Integer -> Integer -> Integer -> String -> String
+getResult options wordCount lineCount charCount size fileName =
+  tab ++ r ++ " " ++ fileName
+  where
+    r = case options of
+      Flags True False False False -> show size
+      Flags False True False False -> show lineCount
+      Flags False False True False -> show charCount
+      Flags False False False True -> show wordCount
+      Flags True True False False -> intercalate tab (show <$> [lineCount, size]) 
+      Flags {} -> intercalate tab (show <$> [lineCount, wordCount, size])
+
+splitOnSpace :: Text -> [Text]
+splitOnSpace = split (\c -> c == ' ' || c == '\t')
+
+countLineWords :: Text -> Int
+countLineWords = length . splitOnSpace
+
+countFileWords :: Int -> Int -> Int -> Handle -> IO (Int, Int, Int)
+countFileWords wordCount lineCount charCount handle = do
+  line <- pack
+    <$> catchIOError
+      (hGetLine handle)
+      (\e -> if isEOFError e
+             then return ""
+             else ioError e)
+  if null line
+    then pure (wordCount, lineCount, charCount)
+    else countFileWords
+      (wordCount + countLineWords line)
+      (succ lineCount)
+      (succ $ length $ unpack line) -- add one for the line break
+      handle
+
